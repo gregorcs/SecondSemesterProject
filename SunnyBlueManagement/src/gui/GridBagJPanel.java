5c9aec3d70f1cd258af9b71876eddaff5c96728d
Gregor Csatlós <g.csatlos1@gmail.com>
2022-02-10 10:15:10 +0100

    Added GridBagLayout JPanels and JFrame

---
+++ SunnyBlueManagement/src/gui/GridBagJPanel.java
@@ -0,0 +1,35 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GridBagJPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public GridBagJPanel() {
+		JButton a, b;
+		setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.anchor = GridBagConstraints.FIRST_LINE_START;
+
+		a = new JButton("button");
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 0;
+		gbc.weighty = 1;
+		add(a, gbc);
+		
+		b = new JButton("button1");
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		add(b, gbc);
+	}
+
+}
---
+++ SunnyBlueManagement/src/gui/MainFrame.java
@@ -0,0 +1,76 @@
+package gui;
+
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.SwingConstants;
+import javax.swing.JLayeredPane;
+import java.awt.CardLayout;
+
+public class MainFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1920, 1080);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
+		mntmNewMenuItem.setHorizontalAlignment(SwingConstants.CENTER);
+		menuBar.add(mntmNewMenuItem);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.rowWeights = new double[]{1.0};
+		gbl_contentPane.columnWeights = new double[]{1.0};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLayeredPane layeredPane = new JLayeredPane();
+		GridBagConstraints gbc_layeredPane = new GridBagConstraints();
+		gbc_layeredPane.fill = GridBagConstraints.BOTH;
+		gbc_layeredPane.gridx = 0;
+		gbc_layeredPane.gridy = 0;
+		contentPane.add(layeredPane, gbc_layeredPane);
+		layeredPane.setLayout(new CardLayout(0, 0));
+		
+		GridBagJPanel panel = new GridBagJPanel();
+		layeredPane.add(panel, "name_1331648275192300");
+		GridBagConstraints gbc = new GridBagConstraints();
+
+		
+	}
+
+}
