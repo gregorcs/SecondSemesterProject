81cbef66917e8c0f44fccc9f23037c46e9b00ff7
Gregor Csatlós <g.csatlos1@gmail.com>
2022-02-10 10:51:26 +0100

    added test statements

--- SunnyBlueManagement/src/Main.java
+++ SunnyBlueManagement/src/Main.java
@@ -1,7 +1,21 @@
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main (String [] args) {
 		System.out.println("hello");
+	}
+
+	/**
+	 * Test statements
+	 */
+	public static void test() {
+		ArrayList<String> examples = new ArrayList<>();
+		examples.add("Potato");
+		examples.add("carrot");
+		examples.add("Coca Cola");
+		examples.add("Champagne");
+		examples.add("Burger");
+		examples.add("Chicken breast");
 	}
 }
