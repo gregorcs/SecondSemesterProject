f2f2f3810b789838ad64adf049558b45589c93d5
Gregor Csatlós <g.csatlos1@gmail.com>
2022-05-18 16:25:09 +0200

    Major refactoring ReserveGUI

--- SunnyBlueManagement/src/gui/reservation/Reserve.java
+++ SunnyBlueManagement/src/gui/reservation/Reserve.java
@@ -6,7 +6,6 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 import java.util.Collection;
 
 import javax.swing.Box;
@@ -14,10 +13,8 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
-import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 import controller.ReservationController;
@@ -31,62 +28,46 @@ import javax.swing.JRadioButton;
 
 public class Reserve extends JPanel {
 
-	private static final long serialVersionUID = 69L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2671428415317456781L;
 	private MainFrame mainFrame;
 	private ReservationController reservationController;
 	private JLayeredPane layeredPane;
+	
 	private JPanel EnterDetailsPanel;
 	private JPanel ChooseTablePanel;
 	private JPanel SpecificRequirementsPanel;
+	
 	private JTextField textName;
 	private JTextField textNumOfPeople;
 	private JTextField textDate;
 	private JTextField textPhoneNum;
+	
 	private TableScrollPane paneTablesAvailable;
 	private TableScrollPane paneTablesSelected;
 	private DecorationScrollPane paneDecorationsAvailable;
 	private DecorationScrollPane paneDecorationsSelected;
+	
 	private JButton btnRemoveDecoration;
 	private JButton btnSelectDecoration;
 	
 	//Panel creation
 	
-	public Reserve (final MainFrame mainFrame) {
-		this.mainFrame = mainFrame;
-		this.reservationController = new ReservationController();
-		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-		
+	private void constructLayeredPane() {
 		layeredPane = new JLayeredPane();
 		add(layeredPane);
 		layeredPane.setLayout(new CardLayout(0, 0));
-		
-		//TODO verify parameters for alignment and positioning
-		EnterDetailsPanel = new JPanel();
-		layeredPane.add(EnterDetailsPanel, "name_28904848226400");
-		EnterDetailsPanel.setLayout(new MigLayout("", "[grow][89px][108.00px][70.00,grow][-109.00]", "[grow][23px][][][][][][][][][grow]"));
-		
-		JLabel lblHeader = new JLabel("Enter Details");
-		lblHeader.setFont(new Font("Tahoma", Font.BOLD, 16));
-		EnterDetailsPanel.add(lblHeader, "cell 1 1,alignx center");
-		
-		Component rigidArea = Box.createRigidArea(new Dimension(20,20));
-		EnterDetailsPanel.add(rigidArea, "cell 1 2,alignx center");
-		
-		paneTablesAvailable = new TableScrollPane();
-		paneTablesSelected = new TableScrollPane();
-		paneDecorationsAvailable = new DecorationScrollPane();
-		paneDecorationsSelected = new DecorationScrollPane();
-		
-		//TEXT FIELDS AND LABELS 
-		
+	}
+	
+	private void createEnterDetailsLabels() {
 		JLabel lblReservationName = new JLabel("Reservation Name:");
 		EnterDetailsPanel.add(lblReservationName, "cell 1 3,alignx trailing");
 				
 		textName = new JTextField();
 		EnterDetailsPanel.add(textName, "cell 2 3,alignx center");
 		textName.setColumns(10);
-		
-		
 		
 		JLabel lblNumOfPeople = new JLabel("No. of people:");
 		EnterDetailsPanel.add(lblNumOfPeople, "cell 1 4,alignx trailing");
@@ -95,8 +76,6 @@ public class Reserve extends JPanel {
 		EnterDetailsPanel.add(textNumOfPeople, "cell 2 4,alignx center");
 		textNumOfPeople.setColumns(10);
 		
-		
-		
 		JLabel lblDate = new JLabel("Date:");
 		EnterDetailsPanel.add(lblDate, "cell 1 5,alignx trailing");
 		
@@ -104,20 +83,19 @@ public class Reserve extends JPanel {
 		EnterDetailsPanel.add(textDate, "cell 2 5,alignx center");
 		textDate.setColumns(10);
 		
-		
-		
 		JLabel lblPhoneNo = new JLabel("Phone Number:");
 		EnterDetailsPanel.add(lblPhoneNo, "cell 1 6,alignx trailing");
 		
 		textPhoneNum = new JTextField();
 		EnterDetailsPanel.add(textPhoneNum, "cell 2 6,alignx center");
 		textPhoneNum.setColumns(10);
-		
+	}
+
+	private void createEnterDetailsButtons() {
 		JButton btnBack = new JButton("Back");
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				mainFrame.backToMainMenu();
-				
 			}
 		});
 		
@@ -160,23 +138,45 @@ public class Reserve extends JPanel {
 				}
 			}
 		});
+		EnterDetailsPanel.add(btnProceed, "cell 2 8,growx");
+		EnterDetailsPanel.add(btnBack, "cell 2 9,growx");
+	}
+	
+	private void constructEnterDetails() {
+		//TODO verify parameters for alignment and positioning
+		EnterDetailsPanel = new JPanel();
+		layeredPane.add(EnterDetailsPanel, "name_28904848226400");
+		EnterDetailsPanel.setLayout(new MigLayout("", "[grow][89px][108.00px][70.00,grow][-109.00]", "[grow][23px][][][][][][][][][grow]"));
+		
+		JLabel lblHeader = new JLabel("Enter Details");
+		lblHeader.setFont(new Font("Tahoma", Font.BOLD, 16));
+		EnterDetailsPanel.add(lblHeader, "cell 1 1,alignx center");
+		
+		Component rigidArea = Box.createRigidArea(new Dimension(20,20));
+		EnterDetailsPanel.add(rigidArea, "cell 1 2,alignx center");
 		
 		Component rigidArea_1 = Box.createRigidArea(new Dimension(20, 20));
 		EnterDetailsPanel.add(rigidArea_1, "cell 2 7");
 		
+		paneTablesAvailable = new TableScrollPane();
+		paneTablesSelected = new TableScrollPane();
+		paneDecorationsAvailable = new DecorationScrollPane();
+		paneDecorationsSelected = new DecorationScrollPane();
 		
+		createEnterDetailsLabels();
+		createEnterDetailsButtons();
+	}
+	
+	
+	private void createReservationCreationLabels() {
+		JLabel lblAvailable = new JLabel("Available");
+		ChooseTablePanel.add(lblAvailable, "cell 0 2");
 		
-		//
-		
-		EnterDetailsPanel.add(btnProceed, "cell 2 8,growx");
-		
-		EnterDetailsPanel.add(btnBack, "cell 2 9,growx");
-		
-		
-		//Table Selection panel
-		ChooseTablePanel = new JPanel();
-		layeredPane.add(ChooseTablePanel, "name_54378967892300");
-		ChooseTablePanel.setLayout(new MigLayout("", "[173.00px,grow][165.00px,center][42.00][264.00,grow][][][]", "[14px][32.00][43.00][184.00,top][][grow]"));
+		JLabel lblSelected = new JLabel("Selected");
+		ChooseTablePanel.add(lblSelected, "cell 3 2");
+	}
+	
+	private void createReservationCreationButtons() {
 		
 		JLabel lblChooseTableHeader = new JLabel("Choose a table");
 		lblChooseTableHeader.setFont(new Font("Tahoma", Font.BOLD, 16));
@@ -207,18 +207,6 @@ public class Reserve extends JPanel {
 		});
 		ChooseTablePanel.add(btnSelectTable, "cell 0 1,alignx left,aligny bottom");
 		ChooseTablePanel.add(btnRemove, "cell 3 1");
-		
-		JLabel lblAvailable = new JLabel("Available");
-		ChooseTablePanel.add(lblAvailable, "cell 0 2");
-		
-		JLabel lblSelected = new JLabel("Selected");
-		ChooseTablePanel.add(lblSelected, "cell 3 2");
-				
-		ChooseTablePanel.add(paneTablesAvailable, "cell 0 3 2 1,grow");
-		ChooseTablePanel.add(paneTablesSelected, "cell 3 3,grow");
-		ChooseTablePanel.add(paneDecorationsSelected, "cell 3 5,grow");
-		ChooseTablePanel.add(paneDecorationsAvailable, "cell 0 5 2 1,grow");
-		
 		
 		//DECORATIONBUTTONS
 		btnSelectDecoration = new JButton("Select");
@@ -276,7 +264,33 @@ public class Reserve extends JPanel {
 		ChooseTablePanel.add(btnConfirmReservation, "cell 6 4");
 		
 	
-		//end of Table Selection
+		
+	}
+	
+	private void constructReservationCreation() {
+		//Table Selection panel
+		ChooseTablePanel = new JPanel();
+		layeredPane.add(ChooseTablePanel, "name_54378967892300");
+		ChooseTablePanel.setLayout(new MigLayout("", "[173.00px,grow][165.00px,center][42.00][264.00,grow][][][]", "[14px][32.00][43.00][184.00,top][][grow]"));
+
+		createReservationCreationLabels();
+				
+		ChooseTablePanel.add(paneTablesAvailable, "cell 0 3 2 1,grow");
+		ChooseTablePanel.add(paneTablesSelected, "cell 3 3,grow");
+		ChooseTablePanel.add(paneDecorationsSelected, "cell 3 5,grow");
+		ChooseTablePanel.add(paneDecorationsAvailable, "cell 0 5 2 1,grow");
+		
+		createReservationCreationButtons();
+	}
+	
+	public Reserve (final MainFrame mainFrame) {
+		this.mainFrame = mainFrame;
+		this.reservationController = new ReservationController();
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		
+		constructLayeredPane();
+		constructEnterDetails();
+		constructReservationCreation();
 		
 		//Specific Requirements Panel
 		//TODO FINISH THIS UP
@@ -304,8 +318,7 @@ public class Reserve extends JPanel {
         //todo edge case handling here
         return Integer.parseInt(input);
 	}
-	
-	
+		
 	private void cleanReservationPanel() {
 		textName.setText("");
         textNumOfPeople.setText("");
