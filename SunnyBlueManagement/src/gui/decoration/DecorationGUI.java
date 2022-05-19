f9a1ac48843db7e8a9ef2b46f2b33ed0f18c8c4c
Gregor Csatlós <g.csatlos1@gmail.com>
2022-05-19 22:27:03 +0200

    Update decorationGui create

--- SunnyBlueManagement/src/gui/decoration/DecorationGUI.java
+++ SunnyBlueManagement/src/gui/decoration/DecorationGUI.java
@@ -14,10 +14,14 @@ import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
 import java.awt.event.ActionListener;
 import java.util.Collection;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import java.awt.FlowLayout;
 
 public class DecorationGUI extends JPanel {
 
@@ -61,6 +65,7 @@ public class DecorationGUI extends JPanel {
 		JButton btnAdd = new JButton("Add decoration");
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				createAskForDecorationInfoDialog();
 			}
 		});
 		panel.add(btnAdd, "cell 4 4");
@@ -108,4 +113,74 @@ public class DecorationGUI extends JPanel {
 		this.repaint();
 	}
 
+	
+	private void createAskForDecorationInfoDialog() {
+		JFrame frame = new JFrame();
+		frame.setBounds(100, 100, 400, 400);
+		JPanel panel = new JPanel();
+		panel.setBounds(100, 100, 400, 400);
+		JButton button = new JButton("create");
+		frame.add(panel);
+		
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		JDialog dialog = new JDialog(frame , "Enter decoration details");
+		dialog.setLayout(new FlowLayout());
+		JLabel lblName = new JLabel("Name: ");
+		JLabel lblDepartment = new JLabel("Department: ");
+		JLabel lblStock = new JLabel("Stock: ");
+		
+		JTextField textFieldName = new JTextField(8);
+		JTextField textFieldDepartment = new JTextField(8);
+		JTextField textFieldStock = new JTextField(8);
+		
+		dialog.add(lblName);
+		dialog.add(textFieldName);
+		dialog.add(lblDepartment);
+		dialog.add(textFieldDepartment);
+		dialog.add(lblStock);
+		dialog.add(textFieldStock);
+		dialog.add(button);
+		dialog.pack();
+		dialog.setVisible(true);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
